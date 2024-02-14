1d8803b8129844a5000c8b76df0aa335e0ada84d
Aakeefah Jones <aakeefahj@gmail.com>
2024-02-14 14:35:01 +0200

    example

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("index file");
+import express from 'express'
+import { Person } from './person.js'
+
+const app = express()
+const router =  express.Router()
+const port = ++process.env.PORT || 4000
+
+app.use(
+    express.json(),
+    router
+)
+router.get('^/$|class', (req, res) =>{
+    res.json({
+        status: res.statusCode,
+        msg: "You are home."
+    })
+})
+router.get('/person1', (req, res) => {
+    let person1 = new Person()
+    person1.firstName = "Shawn"
+    person1.lastName = "Mendes"
+    person1.age = 29
+    person1.display()
+    res.end('Please check the console')
+})
+router.get(/person2/, (req, res) => {
+    let person2 = new Person()
+    person2.firstName = "Layyah"
+    person2.lastName = "Hartley"
+    person2.age = 20
+    person2.display()
+    res.end('Please check the console')
+})
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+})
--- person.js
+++ person.js
@@ -1,1 +1,43 @@
-console.log("person file");
+class Person{
+    static count = 0
+    constructor() {
+// if you don't specify a constructor(allows us to have default values) then the thingy will create it for you.
+        this.firstName = 'Aakeefah',
+        this.lastName = 'Jones',
+        this.age = 0
+        Person.count += 1
+// this.firstName = 'Please enter your first name',
+// this.lastName = 'Please enter your last name',
+    }
+    set FirstName(value) {
+        this.firstName = value
+    }
+    get FirstName() {
+        return this.firstName = value
+    }
+    set LastName(value) {
+        this.lastName = value
+    }
+    get LastName() {
+        return this.lastName = value
+    }
+    set Age(value) {
+        this.age = value
+    }
+    get Age() {
+        return this.age = value
+    }
+    display() {
+        console.log(`
+        ======== Person ID: ${this.count} ========
+            First Name: ${this.firstName}, 
+            Last Name: ${this.lastName},
+            Age: ${this.age}
+        ==========================================
+        `);
+    }
+}
+
+export {
+    Person
+}
